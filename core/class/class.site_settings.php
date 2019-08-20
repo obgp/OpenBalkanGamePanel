5f924717fb6db50ac58317a7ce51102b3895ed1a
RootSec <itznaktuminajasiftw@gmail.com>
2019-08-20 12:06:22 +0200

    Update class.site_settings.php

--- core/class/class.site_settings.php
+++ core/class/class.site_settings.php
@@ -177,4 +177,14 @@ function cryptopubkey() {
 	return $get_site_info['cryptokey'];
 }
 
+function fdllink() {
+
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `site_settings`");
+	$SQLSEC->Execute();
+	$get_site_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
+	return $get_site_info["fdllink"];
+	
+}
+
 ?>
