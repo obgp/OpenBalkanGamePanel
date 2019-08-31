0d7348abdd98af608242f8000727339b1182804b
RootSec <itznaktuminajasiftw@gmail.com>
2019-08-31 12:43:09 +0200

    Prebacivanje celog admin panela u PHP 7 i PDO

--- admin/core/inc/config.php
+++ admin/core/inc/config.php
@@ -25,31 +25,14 @@ ob_start();
 
 date_default_timezone_set("Europe/Belgrade");
 
-define('DB_HOST', 'localhost');
 
-define('DB_USER', 'dev_gb');
-
-define('DB_PASS', 'kolkijeprihod1337');
-
-define('DB_NAME', 'dev_gb');
-
-
-
-if (!$db_connect = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) {
-
-	die("<li> Sorry, site is not connecting to database. </li>");
-
+function rootsec() {
+$servername = "localhost";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DBNAME";
+return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
 }
-
-
-
-if (!mysql_select_db(DB_NAME, $db_connect)) {
-
-	die("<li> Sorry, cannot search to database. </li>");
-
-}
-
-
 
 /**
 
