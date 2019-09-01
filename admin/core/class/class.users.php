ce14d3f6d33e4737e1b0ce4fa174ef14b8bbb1d0
RootSec <itznaktuminajasiftw@gmail.com>
2019-09-02 00:46:23 +0200

    Prebaceno u PDO

--- admin/core/class/class.users.php
+++ admin/core/class/class.users.php
@@ -9,7 +9,11 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/admin/core/inc/config.php');
 */
 
 function is_valid_user($u_id) {
-	$u_info = mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'");
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
+	
 	if (mysql_num_rows($u_info) == 0) {
 		$return = false;
 	} else {
@@ -20,20 +24,26 @@ function is_valid_user($u_id) {
 }
 
 function ban_user($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['banovan']);
 }
 
 function ban_ftp($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['ftp_ban']);
 }
 
 function ban_support($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['support_ban']);
 }
 
@@ -48,20 +58,26 @@ function ban_select($val) {
 }
 
 function user_name($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['username']);
 }
 
 function user_ime($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['ime']);
 }
 
 function user_prezime($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['prezime']);
 }
 
@@ -70,38 +86,50 @@ function user_full_name($u_id) {
 }
 
 function user_email($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['email']);
 }
 
 function user_pincode($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['sigkod']);
 }
 
 function user_token($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['token']);
 }
 
 function user_register($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['kreiran']);
 }
 
 function my_money($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['novac']);
 }
 
 function my_contry($u_id) {
-	$u_info = mysql_fetch_array(mysql_query("SELECT * FROM `klijenti` WHERE `klijentid` = '$u_id'"));
-
+	$rootsec = rootsec();
+	$SQLSEC = $rootsec->prepare("SELECT * FROM `klijenti` WHERE `klijentid` = ?");
+	$SQLSEC->Execute(array($u_id));
+	$u_info = $SQLSEC->fetch(PDO::FETCH_ASSOC);
 	return txt($u_info['zemlja']);
 }
 
