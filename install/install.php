a98b964a6489d79e630fb7211a3fe78318921f83
RootSec <itznaktuminajasiftw@gmail.com>
2019-09-12 23:23:01 +0200

    Create install.php

---
+++ install/install.php
@@ -0,0 +1,47 @@
+<?php
+
+$filename = 'BAZA.sql';
+
+$mysql_host = $_POST["host"];
+$mysql_username = $_POST["username"];
+$mysql_password = $_POST["password"];
+$mysql_database = $_POST["DB"];
+
+if(!$_GET["step"])
+{
+?>
+<title>OBGP Instalacija</title>
+<form method="post" action="/install/install.php?step=2">
+<input name="host" placeholder="HOSTNAME">
+<input name="username" placeholder="USERNAME">
+<input name="password" type="password" placeholder="PASSWORD">
+<input name="DB" placeholder="DBNAME">
+<input type="submit" value="Instaliraj">
+</form>
+
+<?php
+}
+else if ($_GET["step"]==2)
+{
+$con = @new mysqli($mysql_host,$mysql_username,$mysql_password,$mysql_database);
+
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: " . $con->connect_errno;
+    echo "<br/>Error: " . $con->connect_error;
+}
+
+$templine = '';
+$lines = file($filename);
+foreach ($lines as $line) {
+    if (substr($line, 0, 2) == '--' || $line == '')
+        continue;
+
+    $templine .= $line;
+    if (substr(trim($line), -1, 1) == ';') {
+        $con->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . $con->error() . '<br /><br />');
+        $templine = '';
+    }
+}
+echo "Uspesno ste instalirali OBGP, obrisite ovaj folder.";
+$con->close($con);
+}
