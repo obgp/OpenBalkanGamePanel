d028661e2e7cd7fa76db7455c0be850b6e1e5367
RootSec <itznaktuminajasiftw@gmail.com>
2019-09-19 00:59:33 +0200

    Update install.php

--- install/install.php
+++ install/install.php
@@ -14,22 +14,23 @@ $mysql_host = $_POST["host"];
 $mysql_username = $_POST["username"];
 $mysql_password = $_POST["password"];
 $mysql_database = $_POST["DB"];
-$con = @new mysqli($mysql_host,$mysql_username,$mysql_password,$mysql_database);
-if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: " . $con->connect_errno;
-    echo "<br/>Error: " . $con->connect_error;
+
+$sql = file_get_contents($filename);
+
+$mysqli = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+if (mysqli_connect_errno()) { /* check connection */
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
-$templine = '';
-$lines = file($filename);
-foreach ($lines as $line) {
-    if (substr($line, 0, 2) == '--' || $line == '')
-        continue;
-    $templine .= $line;
-    if (substr(trim($line), -1, 1) == ';') {
-        $con->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . $con->error() . '<br /><br />');
-        $templine = '';
-    }
+
+/* execute multi query */
+if ($mysqli->multi_query($sql)) {
+    echo "success";
+} else {
+   echo "error";
 }
+    
+    
 $connect = fopen($_SERVER['DOCUMENT_ROOT']."/core/inc/connect_db.php", "w") or die("Ispraznite bazu stavite i podesite permisije na folder /config/inc/ i pokrenite ponovo instalaciju!");
 $connectadmin = fopen($_SERVER['DOCUMENT_ROOT']."/admin/core/inc/connect_db.php", "w");
 $string = '
