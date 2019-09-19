21dd8cd9f5b92c5ffe397adeccac9cbbeb3f040d
RootSec <itznaktuminajasiftw@gmail.com>
2019-09-19 11:13:30 +0200

    Create db.php

---
+++ core/inc/db.php
@@ -0,0 +1,24 @@
+<?php 
+//ISTA BAZA IDE ZA "rootsec()" i "firewallsec()"
+function rootsec() {
+$servername = "localhost";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DBNAME";
+return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+}
+function firewallsec() {
+$servername = "localhost";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DBNAME";
+return new mysqli($servername, $username, $password, $dbname);
+}
+function masterserver() {
+$servername = "localhost";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DBNAME";
+return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+}
+?>
