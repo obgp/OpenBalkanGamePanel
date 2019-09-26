86edb7339f4ca5e49d1beb34577c827209236156
RootSec <itznaktuminajasiftw@gmail.com>
2019-09-26 12:56:56 +0200

    Create en.php

---
+++ core/lang/en.php
@@ -0,0 +1,20 @@
+<?php  
+/* 
+------------------
+Jezik: Engleski
+------------------
+*/
+
+$jezik = array(); 
+
+$jezik["1"] = "Home";
+$jezik["2"] = "GamePanel";
+$jezik["3"] = "Shop";
+$jezik["4"] = "Forum";
+$jezik["5"] = "GameTracker";
+$jezik["5"] = "Contact";
+$jezik["6"] = "Login";
+$jezik["7"] = "Register";
+$jezik["8"] = "Logout";
+
+?>
