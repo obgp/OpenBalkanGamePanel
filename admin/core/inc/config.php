428147972979ad3b010e70c627fb701e4dd6f3a3
mskoko <52577153+mskoko@users.noreply.github.com>
2019-11-25 06:17:18 +0100

    Update config.php

--- admin/core/inc/config.php
+++ admin/core/inc/config.php
@@ -1,57 +1,46 @@
 <?php
 
+ini_set('display_errors', 1); // Kad bude Live zameni sa 1 => 0;
+ini_set('display_startup_errors', 1); // Kad bude Live zameni sa 1 => 0;
+ini_set('error_log', 'dev/logs/errors.log'); // Logging file path
+error_reporting(E_ALL | E_STRICT | E_NOTICE);
 
-error_reporting(E_ALL);
-
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 ob_start();
 
+date_default_timezone_set('Europe/Belgrade');
 
-
-/**
-
-* Connect Database
-
-* DB_HOST = 'db_host'
-
-* DB_USER = 'db_user'
-
-* DB_PASS = 'db_pass'
-
-* DB_NAME = 'db_name'
-
-*/
-
-date_default_timezone_set("Europe/Belgrade");
-
-
-function rootsec() {
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "obgp";
-return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    // Database for localhost (Not-Live)
+    function rootsec() {
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "";
+        $dbname     = "obgp";
+        return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+    }
+} else {
+    //Database for Live
+    function rootsec() {
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "";
+        $dbname     = "obgp";
+        return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
+    }
 }
 
 /**
-
 * Include file 
-
 */
 
 include_once($_SERVER['DOCUMENT_ROOT'].'/admin/core/inc/inc.php');
 
 
-
 /**
-
 * Admin activity
-
 */
-
 echo admin_activity();
-
-
-
-?>
