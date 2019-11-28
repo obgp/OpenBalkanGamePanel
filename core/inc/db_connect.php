e78582b1d84e1d3267bca6958c2c8ee3b0982af0
RootSec <itznaktuminajasiftw@gmail.com>
2019-11-28 22:08:03 +0100

    Update db_connect.php

--- core/inc/db_connect.php
+++ core/inc/db_connect.php
@@ -1,1 +1,23 @@
-<?php //:) ?>
+<?php
+function rootsec() {	
+$servername = "localhost";	
+$username = "root";	
+$password = "";	
+$dbname = "obgp";	
+return new PDO("mysql:host=".$servername.";dbname=$dbname", $username, $password);	
+}	
+function firewallsec() {	
+$servername = "localhost";	
+$username = "root";	
+$password = "";	
+$dbname = "obgp";	
+return new mysqli($servername, $username, $password, $dbname);	
+}	
+function masterserver() {	
+$servername = "localhost";	
+$username = "USERNAME";	
+$password = "PASSWORD";	
+$dbname = "DBNAME";	
+return new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);	
+}	
+?>
